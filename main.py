643ad09cea87dbbf4d8427ba7f91287d3eced0f3
james townley <james@townley.ca>
2015-04-16 16:34:49 -0600

    1st pass at 1.9 compatibility

--- main.py
+++ main.py
@@ -1,56 +1,33 @@
 from kivy.app import App
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, ObjectProperty
 from kivy.uix.label import Label
 from os.path import join, dirname
 import gettext
 
-class _(str):
-
-    observers = []
-    lang = None
-
-    def __new__(cls, s, *args, **kwargs):
-        if _.lang is None:
-            _.switch_lang('en')
-        s = _.translate(s, *args, **kwargs)
-        return super(_, cls).__new__(cls, s)
-
-    @staticmethod
-    def translate(s, *args, **kwargs):
-        return _.lang(s).format(args, kwargs)
-
-    @staticmethod
-    def bind(**kwargs):
-        _.observers.append(kwargs['_'])
-
-    @staticmethod
-    def switch_lang(lang):
-        # get the right locales directory, and instanciate a gettext
-        locale_dir = join(dirname(__file__), 'data', 'locales')
-        locales = gettext.translation('langapp', locale_dir,
-            languages=[lang])
-        _.lang = locales.gettext
-
-        # update all the kv rules attached to this text
-        for callback in _.observers:
-            callback()
-
 
 class MySpecialLabel(Label):
     def __init__(self, **kwargs):
+        self.app = App.get_running_app()
+        self.app.bind(_=self.on_lang)
         super(MySpecialLabel, self).__init__(**kwargs)
-        _.bind(_= self.on_lang)
 
-    def on_lang(self):
-        self.text = _(u'Running')
-
+    def on_lang(self, instance, value):
+            self.text = self.app._('Running')
 
 class LangApp(App):
-
     lang = StringProperty('en')
+    _ = ObjectProperty(None, allownone=True)
+
+    def build(self):
+        self.switch_lang(self.lang)
 
     def on_lang(self, instance, lang):
-        _.switch_lang(lang)
+        self.switch_lang(lang)
+
+    def switch_lang(self, lang):
+        locale_dir = join(dirname(__file__), 'data', 'locales')
+        locales = gettext.translation('langapp', locale_dir, languages=[self.lang])
+        self._ = locales.ugettext
 
 
 LangApp().run()
